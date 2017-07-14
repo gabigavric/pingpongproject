aeef233d53cc1d3508644bc91f3424303d5fb7f5
gabrijela <gabigavric@gmail.com>
2017-07-13 20:37:10 -0700

    no readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
-$(document).ready(function) {
-  if ()
+$(document).ready(function(){
+
+  $("form").submit(function(event) {
+
+    var theirNumber = $('input#number')
+    var value = $( "input#number" ).val();
+
+    console.info("user number is: " + value);
+
+    for(i=1;i<=value;i++)
+    {
+      console.info(i);
+      if(i % 15 == 0){
+        $("ul#output").append($("<li>").text("pingpong"));
+        console.info(i+ "pingping");
+      }else if(i % 5 == 0){
+        $("ul#output").append($("<li>").text("pong"));
+        console.info(i+ "pong");
+      }else if(i % 3 ==0){
+        $("ul#output").append($("<li>").text("ping"));
+        console.info(i+"ping");
+      }else{
+        $("ul#output").append($("<li>").text(i))
+      }
+
+    }
+
+    event.preventDefault();
+  });
 
 
 
