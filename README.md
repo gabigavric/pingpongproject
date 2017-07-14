6bf7e97a8a37a2eefaf465a6e221a73a2bc0c7fe
gabrijela <gabigavric@gmail.com>
2017-07-13 21:58:45 -0700

    will made readme

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1>A Game Of Ping-Pong!</hi>
+<h3><u>Author</u></h3>
+<p>Gabrijela Gavric</p>
+<h3><u>Link to github</u></h3>
+<p>INSERT---</p>
+<h3><u>Specifications</u><h3>
+<p>It can count up to the provided number.</p>
+<ul>
+  <li>Example Input: 2</li>
+  <li>Example Output: [1,2]</li>
+</ul>
+<p>It replaces any number divisble by 3 with the word "Ping" and any number divisible by 5 with
+the word "Pong". Then, if a number is divisble by 15, it displays "Ping-Pong".
+<h3><u>Built With<u><h3>
+<ul>
+  <li>HTML</li>
+  <li>CSS<li>
+  <li>Bootstrap</li>
+  <li>JavaScript</li>
+  <li>JQuery</li>
+</ul>
--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $("form").submit(function(event) {
 
     var theirNumber = $('input#number')
-    var value = $( "input#number" ).val();
+    var value = $("input#number").val();
 
     console.info("user number is: " + value);
 
-    for(i=1;i<=value;i++)
-    {
+    for (i = 1; i <= value; i++) {
       console.info(i);
-      if(i % 15 == 0){
+      if (i % 15 == 0) {
         $("ul#output").append($("<li>").text("pingpong"));
-        console.info(i+ "pingping");
-      }else if(i % 5 == 0){
+        console.info(i + "pingping");
+      } else if (i % 5 == 0) {
         $("ul#output").append($("<li>").text("pong"));
-        console.info(i+ "pong");
-      }else if(i % 3 ==0){
+        console.info(i + "pong");
+      } else if (i % 3 == 0) {
         $("ul#output").append($("<li>").text("ping"));
-        console.info(i+"ping");
-      }else{
+        console.info(i + "ping");
+      } else {
         $("ul#output").append($("<li>").text(i))
       }
 
